4b2a3e8c0d2060b28e3ddcc2db95d04b175ddb15
SchraderZhang <zph594972837@gmail.com>
2024-10-08 22:36:05 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "隔壁老张的栗子猫猫",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "隔壁老张的栗子猫猫",
     statusBarStyle: "default",
   },
 };
